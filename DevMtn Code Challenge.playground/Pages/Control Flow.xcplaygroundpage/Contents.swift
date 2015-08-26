7cbe51fa940eb989103e0e7f5bbf61e1167cf0e9
Caleb Hicks <calebhicks@gmail.com>
2015-08-26 12:00:44 -0600

    adds section for demoing control flow challenge

--- DevMtn Code Challenge.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ DevMtn Code Challenge.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -5,5 +5,8 @@
 Write a function that prints the numbers from 1 to 100. But for multiples of three print “Dev” instead of the number and for the multiples of five print “Mountain”. For numbers which are multiples of both three and five print “DevMountain”.
 */
 
+//: Demo the use of your function here. Go to View -> Debug Area -> Show Debug Area to see the logged results.
+
+
 //: [Previous](@previous)
 //: [Next](@next)
