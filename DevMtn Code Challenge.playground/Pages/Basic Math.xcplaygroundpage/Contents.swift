242a91c0c5fe5baaef92160eaadbb80f8b8e5313
Caleb Hicks <calebhicks@gmail.com>
2015-08-26 11:28:20 -0600

    cleans up whitespace, changes to rendered markup

--- DevMtn Code Challenge.playground/Pages/Basic Math.xcplaygroundpage/Contents.swift
+++ DevMtn Code Challenge.playground/Pages/Basic Math.xcplaygroundpage/Contents.swift
@@ -3,15 +3,13 @@
 
 Demonstrate basic knowledge of functions that take parameters and return results by implementing some basic math functions.
 */
-
-
 //: Write a function that returns the sum of two numbers
 
 
 //: Write a function that returns the product of two numbers
 
 
-//: Write a function that returns the average of an array of numbers
+//: Write a function that returns the average (mean) of an array of numbers
 
 
 //: Demo the use of your functions here:
