02e9b0f78d5ba3a7d93dabcf40956b5e849bb40d
Caleb Hicks <calebhicks@gmail.com>
2015-08-27 11:39:30 -0600

    more language tweaks

--- DevMtn Code Challenge.playground/Pages/Conclusion.xcplaygroundpage/Contents.swift
+++ DevMtn Code Challenge.playground/Pages/Conclusion.xcplaygroundpage/Contents.swift
@@ -1,8 +1,8 @@
 /*:
 
-## Thank You
+## All Done!
 
-Thank you for submitting your application and completing your DevMountain iOS Code Challenge. Go to the [DevMountain iOS Code Challenge Submission Form](https://www.dropbox.com/request/WOiteuvkN39GHx2MDe4A) on Dropbox to upload this file.
+Nice work. Now you've finished, go to the [DevMountain iOS Code Challenge Submission Form](https://www.dropbox.com/request/WOiteuvkN39GHx2MDe4A) on Dropbox to upload this file.
 
 Mentors review submissions twice each week. If you have any questions about your application before you hear back, please contact interviews@devmountain.com.
 
