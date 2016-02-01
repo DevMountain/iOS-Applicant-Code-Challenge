e7ce2a0d1ca523efbfb41934f41bd0ffd5f872ac
Diego-F-Aguirre <diego3884@gmail.com>
2016-02-01 14:02:47 -0700

    code challenge complete

--- DevMtn Code Challenge.playground/Pages/About Me.xcplaygroundpage/Contents.swift
+++ DevMtn Code Challenge.playground/Pages/About Me.xcplaygroundpage/Contents.swift
@@ -3,7 +3,11 @@
 
 Tell us a little bit about yourself using Swift variables. Start with your first name, your last name, your age, where you're from, and why you want to take the class.
 */
-
+let firstName = "Diego"
+let lastName = "Aguirre"
+let age = 31
+let address = "Waltham, MA"
+var myWhy = "I always was fascinated with people who could design their own apps and always day dreamed about doing it myself. Recently since my career was not going anywhere I decided to take the plunge and go for a career change into programming"
 
 //: Create an array of Strings that holds a few of your hobbies.
 
@@ -12,10 +16,10 @@ Tell us a little bit about yourself using Swift variables. Start with your first
 
     For example: "Programming, Teaching, Golf, and Basketball."
 */
-
+var hobbies = ["Crossfit", "videogames", "reading", "photoshop"]
 
 //: Using the variables you have created, write a programmatically generated sentence to introduce yourself. Use only one print() statement.
 
-
+print("Hi my name is \(firstName) \(lastName) and I'm a \(age) year old male from \(address). \(myWhy)")
 //: [Previous](@previous)
 //: [Next](@next)
--- DevMtn Code Challenge.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
+++ DevMtn Code Challenge.playground/Pages/Control Flow.xcplaygroundpage/Contents.swift
@@ -4,9 +4,23 @@
 
 Write a function that prints the numbers from 1 to 100. But for multiples of three print “Dev” instead of the number and for the multiples of five print “Mountain”. For numbers which are multiples of both three and five print “DevMountain”.
 */
-
+func DevMountain() {
+ 
+    for number in 1...100 {
+        if number % 3 == 0 && number % 5 == 0 {
+            print("DevMountain")
+        } else if number % 3 == 0 {
+            print("Dev")
+        } else if number % 5 == 0 {
+            print("Mountain")
+        } else {
+            print(number)
+        }
+    }
+    
+}
 //: Demo the use of your function here. Go to View -> Debug Area -> Show Debug Area to see the logged results.
 
-
+DevMountain()
 //: [Previous](@previous)
 //: [Next](@next)
